d78f63f3012a6edc809f4d81bd8f08924cf140ea
Cody <codysapp95@gmail.com>
2020-08-13 21:50:00 -0400

    Created ReportingStructure.cs
    Created ReportingStructure.cs with attributes and some documentation.

---
+++ code-challenge/Models/ReportingStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+    /// <summary>
+    /// ReportingStructure.cs includes ReportStructure
+    /// class that contains the Employee of focus
+    /// and the number of employees whom report to
+    /// the Employee of focus.
+    /// </summary>
+    public class ReportingStructure
+    {
+        /// <summary>
+        /// This attribute contains the Employee object of focus.
+        /// </summary>
+        /// <see cref="Models.Employee"/>
+        public Employee Employee { get; set; }
+
+        /// <summary>
+        /// NumberOfReports attribute contains the number of employees
+        /// whom report to the Employee of focus, including those whom
+        /// report to the reportees and so forth.
+        /// </summary>
+        public int NumberOfReports { get; set; }
+
+
+    }
+}
