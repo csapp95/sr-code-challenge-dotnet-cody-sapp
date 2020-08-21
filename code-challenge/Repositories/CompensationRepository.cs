a0f14d9525f9a01293a75787af7590d521dbf6f7
Cody <codysapp95@gmail.com>
2020-08-20 21:49:21 -0400

    Code Cleanup

--- code-challenge/Repositories/CompensationRepository.cs
+++ code-challenge/Repositories/CompensationRepository.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 
 namespace challenge.Repositories
 {
+    /// <summary>
+    /// CompensationRepository implements ICompensationRepository
+    /// </summary>
     public class CompensationRepository :ICompensationRepository
     {
 
@@ -21,25 +24,25 @@ namespace challenge.Repositories
             this._logger = logger;
         }
 
+        /// <summary>
+        /// Gets compensation object associated with the given id if one exists.
+        /// </summary>
+        /// <param name="id">EmployeeId string</param>
+        /// <returns></returns>
         public Compensation GetById(String id)
         {
             return _compensationContext.CompensationRecords.SingleOrDefault(c => c.Employee.EmployeeId == id);
         }
 
+        /// <summary>
+        /// Adds compensation object to the context.
+        /// </summary>
+        /// <param name="compensation"></param>
+        /// <returns></returns>
         public Compensation Add(Compensation compensation)
         {
-            Compensation addedComp = compensation;
-            try
-            {
-                _compensationContext.CompensationRecords.Add(compensation);
-            }
-            catch(Exception e)
-            {
-                _logger.LogError($"Error Adding Compensation for employee id {compensation.Employee.EmployeeId}. \nMsg:{e.Message} \nTrace:{e.StackTrace}");
-                addedComp = null;
-            }
-
-            return addedComp;
+            _compensationContext.CompensationRecords.Add(compensation);
+            return compensation;
 
         }
 
@@ -48,6 +51,11 @@ namespace challenge.Repositories
             return _compensationContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Removes the given compensation object.
+        /// </summary>
+        /// <param name="compensation"></param>
+        /// <returns></returns>
         public Compensation Remove(Compensation compensation)
         {
             return _compensationContext.CompensationRecords.Remove(compensation).Entity;
